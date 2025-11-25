33af60c44362e53a0e4504a5fb444836bb1c0bef
VivekCoderze <dayanandgarg967@gmail.com>
2025-11-25 10:53:33 +0530

    add probems

--- Petya_and_Strings.cpp
+++ Petya_and_Strings.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void  toLowerCase(string &s){
+    for(int i = 0;i<s.length();i++){
+        if(s[i]>='A' && s[i]<='Z') s[i]+=32;
+    }
+}
 
 int main()
 {
     string s1, s2;
     cin >> s1 >> s2;
-    int n1 = 0, n2 = 0;
-    for (int i = 0; i < s1.size(); i++)
-    {
-        n1+= tolower(s1[i]);
-        n2+= tolower(s2[i]);
-    }
-    if(n1>n2) cout<<1;
-    else if(n1<n2) cout<<-1;
-    if(n1==n2) cout<<0;
+    toLowerCase(s1);
+    toLowerCase(s2);
+    if(s1>s2) cout<<1;
+    else if(s1<s2) cout<<-1;
+    else if(s1==s2) cout<<0;
 }
